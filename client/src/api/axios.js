ef013f159b1ddde59275fef3a3dafffcc71fd929
Abhay17R <abhayjh132@gmail.com>
2025-07-10 13:02:15 +0545

    api_corrected

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Ek naya axios instance banate hain
 const API = axios.create({
   // Vite ke tareeke se environment variable ko access karein
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL+'/api/v1',
   
   withCredentials: true, 
 });
